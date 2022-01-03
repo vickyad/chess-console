59253ba517f485355f9fb62ce890b2f3ee29701f
Victoria <victoria.ad40@gmail.com>
2022-01-03 15:29:37 -0300

    feat: class ChessPosition added

---
+++ chess-console/chess/ChessPosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using chess_console.board;
+
+namespace chess_console.chess
+{
+    class ChessPosition
+    {
+        public int Line { get; set; }
+        public char Column { get; set; }
+
+        public ChessPosition(int line, char column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{Column}{Line}";
+        }
+    }
+}
